e9d483f01cddd9cbf5bd1158cdf94abaca09d96c
Mike <korostelevm@gmail.com>
2023-01-24 10:59:08 -0500

    add registerj.js

---
+++ register.js
@@ -0,0 +1,46 @@
+const APPLICATION_ID = process.env.DISCORD_APPLICATION_ID 
+const TOKEN = process.env.token 
+const GUILD_ID = process.env.GUILD_ID 
+
+
+const axios = require('axios')
+
+const discord_api = axios.create({
+  baseURL: 'https://discord.com/api/',
+  timeout: 3000,
+  headers: {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+	"Access-Control-Allow-Headers": "Authorization",
+	"Authorization": `Bot ${TOKEN}`
+  }
+});
+
+const run = async function(){
+
+    let slash_commands = [
+        {
+          "name": "submit",
+          "description": "",
+          "options": []
+        },
+      ]
+      try
+      {
+        let discord_response = await discord_api.put(
+          `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
+          slash_commands
+        )
+        console.log(discord_response.data)
+        console.log('commands have been registered')
+      }catch(e){
+        console.error(e.code)
+        console.error(e.response?.data)
+        console.log(`${e.code} error from discord`)
+      }
+    
+    
+}
+
+
+run()
