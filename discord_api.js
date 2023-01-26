099413d8ccff43acd0e9da64c0fe8baba3983b9b
Mike <korostelevm@gmail.com>
2023-01-26 14:35:24 -0500

    new code

---
+++ discord_api.js
@@ -0,0 +1,18 @@
+const TOKEN = process.env.token 
+
+
+const axios = require('axios')
+
+const discord_api = axios.create({
+  baseURL: 'https://discord.com/api/',
+  timeout: 3000,
+  headers: {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+	"Access-Control-Allow-Headers": "Authorization",
+	"Authorization": `Bot ${TOKEN}`
+  }
+});
+
+
+module.exports = discord_api
